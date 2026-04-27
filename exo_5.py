67f9392b94a101c728d7d1a337328391d70b2401
Clementine <clementine.dewerchin@gmail.com>
2026-04-27 13:56:21 +0200

    exo_5.py

--- exo_5.py
+++ exo_5.py
@@ -1,13 +1,17 @@
-numbers_list = [1, 2, ]
-i = 0
-
 length_list = int(input("De quelle longueur doit être la liste ? "))
+
 if length_list <= 1:
     length_list = int(input("Ta réponse doit être supérieure à 1 : "))
 
-for _ in range(length_list - 2):
-    next_number = numbers_list[i] + numbers_list[i + 1]
-    numbers_list.append(next_number)
-    i += 1
+numbers = "1, 2"
+first_number = 1
+second_number = 2
 
-print(numbers_list)
+for _ in range(length_list - 2):
+    next_number = first_number + second_number
+    numbers += f", {next_number}"
+
+    first_number = second_number
+    second_number = next_number
+
+print(numbers)
