fedab088bf0a87a80f28885bcf2494575c90be13
Clementine <clementine.dewerchin@gmail.com>
2026-04-27 13:57:18 +0200

    exo_2.py

--- exo_2.py
+++ exo_2.py
@@ -1,9 +1,10 @@
 def multiplication_table():
     table = int(input("Choisis une table : "))
+    
     while table > 10 or table < 1:
         table = int(input("Ton nombre doit être compris entre 1 et 10 : "))
-    for n in range(1, 11):
-        result = table * n
-        print(str(n) + " x " + str(table) + " = " + str(result))
 
-result_table = multiplication_table()
+    for n in range(1, 11):
+        print(f"{n} x {table} = {n * table}")
+
+multiplication_table()
