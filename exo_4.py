7a671ffd681f4a2a8f27da2e22874f4f9503756d
Clementine <clementine.dewerchin@gmail.com>
2026-04-27 13:55:30 +0200

    exo_4.py

--- exo_4.py
+++ exo_4.py
@@ -1,31 +1,26 @@
 
+from random import randint
+
 def sum_user_numbers():
     numb_list = []
     result = 0
     count = 1
 
-    n = int(input("Combien de chiffres vas-tu devoir additionner : "))
-    while n > 20 or n < 0:
-        n = int(input("Ta réponse doit se trouver entre 0 et 20 : "))
+    n = int(input("Choisis un nombre de chiffres à faire apparaitre. Ta réponse doit se situer entre 2 et 5 : "))
+    while n > 5 or n < 2:
+        n = int(input("Le nombre de chiffres doit se trouver entre 2 et 5 : "))
 
     for _ in range(n):
-        user_numb = int(input("Choisis un chiffre entre 2 et 5 : "))
-        while user_numb > 5 or user_numb < 2:
-            user_numb = int(input("Ton chiffre doit se trouver entre 2 et 5 : "))
-        numb_list.append(user_numb) 
-        result += user_numb  
+        random_number = randint(0,20)
+        numb_list.append(random_number) 
+        result += random_number 
 
-
-    user_result = int(input("Quelle est la somme de tous tes nombres " + str(numb_list) + " ? " ))
+    user_result = int(input(f"Quelle est la somme de tous tes nombres {numb_list} ? " ))
     
     while user_result != result:
-        user_result = int(input("Malheureusement, ta réponse n'est pas correcte 😒 Essaye encore " + str(numb_list) + " : "))
+        user_result = int(input(f"Malheureusement, ta réponse n'est pas correcte 😒 Essaye encore {numb_list} : "))
         count += 1
     if user_result == result:
-        print("Yeah ! Tu as trouvé directement 🏆 Tu as trouvé au bout de " + str(count) + " tentative(s).")
-
-
-
-
+        print(f"Yeah ! Tu as trouvé directement 🏆 Tu as trouvé au bout de {count} tentative(s).")
 
 sum_game = sum_user_numbers()
