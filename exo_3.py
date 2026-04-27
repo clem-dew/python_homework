6d7401c02035ae6e6220dbdea2c3479e07e19e9c
Clementine <clementine.dewerchin@gmail.com>
2026-04-27 13:56:53 +0200

    exo_3.py

--- exo_3.py
+++ exo_3.py
@@ -3,20 +3,23 @@ from random import randint
 VALUES = ["pierre", "papier", "ciseaux"]
 
 def game():
-    choice = int(input("Choisis un chiffre entre 1 et 3 : "))
+    print("1 = pierre, 2 = papier, 3 = ciseaux")
+    user_choice = int(input("Choisis un chiffre entre 1 et 3 : "))
 
-    while choice > 3 or choice < 1:
-        choice = int(input("Ton chiffre doit être 1, 2 ou 3 : "))
+    while user_choice > 3 or user_choice < 1:
+        print("1 = pierre, 2 = papier, 3 = ciseaux")    
+        user_choice = int(input("Ton chiffre doit se trouver entre 1 et 3 : "))
 
-    random_choice = randint(1, 3)
-    user_choice = VALUES[choice - 1]
-    pc_choice = VALUES[random_choice - 1]
+    pc_choice = randint(0, 2)
+    user_choice -= 1
 
-    if choice == random_choice:
-        print("Vous : " + user_choice + " | PC : " + pc_choice + " | Egalité")
-    elif (choice == 1 and random_choice == 3) or (choice == 2 and random_choice == 1) or (choice == 3 and random_choice == 2):
-        print("Vous : " + user_choice + " | PC : " + pc_choice + " | Vous avez gagné 🏆")
+    message = f"Vous : : {VALUES[user_choice]} | PC : : {VALUES[pc_choice]}"
+
+    if user_choice == pc_choice:
+        print(f"{message} | Egalité")
+    elif user_choice == 1 and pc_choice == 3 or user_choice == 2 and pc_choice == 1 or user_choice == 3 and pc_choice == 2:
+        print(f"{message} | Vous avez gagné 🏆")
     else:
-        print("Vous : " + user_choice + " | PC : " + pc_choice + " | Vous avez perdu ... 😒")
+        print(f"{message} | Vous avez perdu ... 😒")
 
 start_game = game()
